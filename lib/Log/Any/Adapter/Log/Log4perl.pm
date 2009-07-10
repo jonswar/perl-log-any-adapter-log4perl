897c8bd3dce0773acc5c510863fc3bff07e54608
Jonathan Swartz <swartz@pobox.com>
2009-07-10 16:01:35 -0700

    delete unneeded files; add dependencies and module and test file

--- lib/Log/Any/Adapter/Log/Log4perl.pm
+++ lib/Log/Any/Adapter/Log/Log4perl.pm
@@ -1,9 +1,26 @@
 package Log::Any::Adapter::Log::Log4perl;
-use 5.006;
+use Log::Log4perl;
 use strict;
 use warnings;
+use base qw(Log::Any::Adapter::Base);
 
-our $VERSION = '0.01';
+sub init {
+    my ($self) = @_;
+
+    $self->{logger} = Log::Log4perl->get_logger( $self->{category} );
+}
+
+# Delegate methods to logger, mapping levels down to log4perl levels where necessary
+#
+foreach my $method ( Log::Any->logging_and_detection_methods() ) {
+    my $log4perl_method = $method;
+    for ($log4perl_method) {
+        s/notice/info/;
+        s/warning/warn/;
+        s/critical|alert|emergency/fatal/;
+    }
+    __PACKAGE__->delegate_method_to_slot( 'logger', $method, $log4perl_method );
+}
 
 1;
 
@@ -13,33 +30,42 @@ __END__
 
 =head1 NAME
 
-Log::Any::Adapter::Log::Log4perl -- Adapter to use Log::Log4perl with Log::Any
+Log::Any::Adapter::Log::Log4perl
 
 =head1 SYNOPSIS
 
-    use Log::Any::Adapter::Log::Log4perl;
+    use Log::Log4perl;
+    Log::Log4perl::init('/etc/log4perl.conf');
+    Log::Any->set_adapter('Log::Log4perl');
 
 =head1 DESCRIPTION
 
-Log::Any::Adapter::Log::Log4perl provides
+This Log::Any adapter uses L<Log::Log4perl|Log::Log4perl> for logging. log4perl
+must be initialized before calling I<set_adapter>. There are no parameters.
+
+=head1 LOG LEVEL TRANSLATION
+
+Log levels are translated from Log::Any to Log4perl as follows:
+
+    notice -> info
+    warning -> warn
+    critical -> fatal
+    alert -> fatal
+    emergency -> fatal
+
+=head1 SEE ALSO
+
+L<Log::Any|Log::Any>, L<Log::Log4perl|Log::Log4perl>
 
 =head1 AUTHOR
 
 Jonathan Swartz
 
-=head1 SEE ALSO
-
-L<Some::Module>
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright (C) 2007 Jonathan Swartz.
+Copyright (C) 2007 Jonathan Swartz, all rights reserved.
 
-Log::Any::Adapter::Log::Log4perl is provided "as is" and without any express or implied warranties, including, without
-limitation, the implied warranties of merchantibility and fitness for a particular
-purpose.
-
-This program is free software; you can redistribute it and/or modify it under the same
-terms as Perl itself.
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
 
 =cut
--- t/Sanity.t
+++
@@ -1,3 +0,0 @@
-#!perl -w
-use Log::Any::Adapter::Log::Log4perl::t::Sanity;
-Log::Any::Adapter::Log::Log4perl::t::Sanity->runtests;
